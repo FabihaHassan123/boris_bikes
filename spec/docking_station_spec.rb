b4a705ad7db428958e96f97af16aacc3f422a3d9
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-10-13 14:44:20 +0100

    added dock method to docking_station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,6 +10,13 @@ require "docking_station"
       it "return a working instance of the bike class" do
         expect(subject.release_bike.working?).to eq true
       end
+    end
 
+    describe "#dock" do
+      it "adds a bike to the docking station" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bikes.include?(bike)).to eq(true)
+      end
     end
   end
