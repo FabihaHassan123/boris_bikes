140c4db80d8a312ad7c7dee137fc279ecebfe36d
Fabiha <fabiha.hassan96@gmail.com>
2020-10-13 17:00:08 +0100

    mock behaviour by allowing doubles to respond to certain methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,5 +4,11 @@ describe Bike do
     it "responds to the working? method" do
       expect(subject).to respond_to(:working?)
     end
+    it "responds true if bike is working" do
+      expect(Bike.new(true).working?).to eq(true)
+    end
+    it "responds false if bike is not working" do
+      expect(Bike.new(false).working?).to eq(false)
+    end
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,16 @@
 require "docking_station"
 
+
+
   describe DockingStation do
+
+    let(:bike) {double :bike}
+
+    before(:each) do
+      allow(bike).to receive(:working?).and_return(true)
+      allow(bike).to receive(:working=).and_return(true)
+    end
+
     it "has a capacity equal to 20 by default" do
       expect(subject.capacity).to eq(20)
     end
@@ -15,7 +25,7 @@ require "docking_station"
       end
 
       it "return a working instance of the bike class" do
-        subject.dock(Bike.new)
+        subject.dock(bike)
         expect(subject.release_bike.working?).to eq(true)
       end
 
@@ -24,28 +34,27 @@ require "docking_station"
       end
 
       it "will only release bikes that are working" do
-        subject.dock(Bike.new(), false)
+        allow(bike).to receive(:working?).and_return(false)
+        subject.dock(bike, false)
         expect{subject.release_bike}.to raise_error("All bikes currently out of service")
       end
     end
 
     describe "#dock" do
       it "adds a bike to the docking station" do
-        bike = Bike.new
         subject.dock(bike)
         expect(subject.bikes.include?(bike)).to eq(true)
       end
       it "raises an error if docking_station is at capacity" do
-        subject.capacity.times {subject.dock(Bike.new)}
-        expect{subject.dock(Bike.new)}.to raise_error("Docking Station is at capacity")
+        subject.capacity.times {subject.dock(bike)}
+        expect{subject.dock(bike)}.to raise_error("Docking Station is at capacity")
       end
       it "takes a working? argument and applies this to the instance of Bike being checked in" do
-        bike = Bike.new
         subject.dock(bike, true)
         expect(subject.bikes[0].working?).to eq(true)
       end
       it "takes a working? argument and applies this to the instance of Bike being checked in" do
-        bike = Bike.new
+        allow(bike).to receive(:working?).and_return(false)
         subject.dock(bike, false)
         expect(subject.bikes[0].working?).to eq(false)
       end
